c152f2134893866adb10bc8b390d9fea7a5387b9
CelsoMartin <c.martin01@ufromail.cl>
2017-06-12 10:10:36 -0400

    Segunda version

--- src/main/java/madpa/app/Aplicacion.java
+++ src/main/java/madpa/app/Aplicacion.java
@@ -3,15 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package madpa.ejecucion;
+package madpa.app;
 
-import madpa.menus.MenuPrincipal;
+import madpa.gui.menu.Principal;
 /**
  *
  * @author Celso
  */
 public class Aplicacion {
     public static void main(String[] args) {
-        MenuPrincipal m = new MenuPrincipal();
+        Principal menu = new Principal();
     }
 }
